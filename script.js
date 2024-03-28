d253d4a57230b0852d184fcddececc031f70bd48
Anastasiya Kubatina <82117054+kybikn@users.noreply.github.com>
2024-03-28 23:50:53 +0300

    feat: add slider

--- script.js
+++ script.js
@@ -122,3 +122,82 @@ buttonSignUp.addEventListener("click", alertSignUp);
 buttonPassword.addEventListener("click", alertPassword);
 
 
+// -------------------------------------------------------------------
+// const point = document.querySelectorAll(".slider__img");
+// const imageSlider = document.querySelectorAll(".circles__dot");
+// const leftBtn = document.querySelector(".arrows__left");
+// const rightBtn = document.querySelector(".arrows__right");
+
+
+let point = document.querySelectorAll('.circles__dot')
+let imageSlider = document.querySelectorAll('.imageSlider')
+let leftBtn = document.getElementById('leftBtn')
+let rightBtn = document.getElementById('rightBtn')
+
+point[0].classList.add('activeImage')
+imageSlider[0].classList.add('activeImage')
+
+let counter = 0;
+
+for (let i = 0; i < point.length; i++) {
+  point[i].addEventListener('click', () => {
+    for (let k = 0; k < imageSlider.length; k++) {
+      point[k].classList.remove('activeImage')
+      imageSlider[k].classList.remove('activeImage')
+    }
+    counter = i;
+    imageSlider[counter].classList.add('activeImage');
+    point[counter].classList.add('activeImage');
+  })
+}
+
+leftBtn.addEventListener('click', () => {
+  for (let k = 0; k < imageSlider.length; k++) {
+    point[k].classList.remove('activeImage')
+    imageSlider[k].classList.remove('activeImage')
+  }
+  counter--
+  if (counter < 0) {
+    counter = imageSlider.length - 1
+  }
+  imageSlider[counter].classList.add('activeImage');
+  point[counter].classList.add('activeImage');
+})
+
+rightBtn.addEventListener('click', () => {
+  for (let k = 0; k < imageSlider.length; k++) {
+    point[k].classList.remove('activeImage')
+    imageSlider[k].classList.remove('activeImage')
+  }
+  counter++
+  if (counter >= imageSlider.length) {
+    counter = 0
+  }
+  imageSlider[counter].classList.add('activeImage');
+  point[counter].classList.add('activeImage');
+})
+
+
+function slowSlider() {
+  for (let k = 0; k < imageSlider.length; k++) {
+    point[k].classList.remove('activeImage')
+    imageSlider[k].classList.remove('activeImage')
+  }
+  counter++
+  if (counter >= imageSlider.length) {
+    counter = 0
+  }
+  imageSlider[counter].classList.add('activeImage');
+  point[counter].classList.add('activeImage');
+}
+
+
+
+
+
+
+
+
+
+
+
